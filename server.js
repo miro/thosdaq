cf2feeb07a4faa1d2d16650f5f05775f8cceaa79
Miro Nieminen <miro.nieminen@futurice.com>
2014-05-10 00:35:55 +0300

    Remove unneeded function from the server

--- server.js
+++ server.js
@@ -9,30 +9,8 @@ app.use(logger("tiny"));
 app.use(bodyParser());
 app.use(express.static(__dirname + '/app')); // static file hosting middleware
 
-// Mock route for mock data
-app.get('/api/mock/:filename', function(req, res) {
-	var fs = require('fs');
-    var path = require('path');
-    var filename = req.params.filename;
 
-    if(!filename) {
-        res.send({ msg: 'Filename must be specified' });
-        return;
-    }
-
-    // Server must be started in project's root!
-    fs.readFile(path.resolve(process.cwd(), 'mockdb/' + filename), function(error, file) {
-        if(error) {
-            res.send({ msg: 'Could not read ' + filename, error: error });
-            return;
-        }
-        res.send(file);
-    });
-});
-
-
-
-app.post('/drink', function(req, res) {
+app.post('/log', function(req, res) {
     console.log("drink post");
     console.dir(req.body);
     res.send(req.body);
@@ -43,7 +21,7 @@ app.post('/drink', function(req, res) {
 // Launch server
 var port = 3000;
 var server = app.listen(port, function() {
-	console.log("Server running :: http://localhost:" + port);
+    console.log("Server running :: http://localhost:" + port);
 });
 
 exports.server = server;
